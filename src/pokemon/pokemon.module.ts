94bb6b892a7722dcef79b7d3b97c8b312fcec76a
Otto <ogutierrezgz.94@hotmail.com>
2023-05-10 01:32:08 -0600

    CRUD completo

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name, //name no es la variable declarada en la entity, es el nombre de la clase
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
+})
+export class PokemonModule {}
